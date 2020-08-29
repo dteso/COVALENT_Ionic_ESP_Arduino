e816e6ec454dd7a583cbf06de77dba6b5450718b
dteso <dtesodev@gmail.com>
2020-08-29 14:15:14 +0200

    serial terminal reading data from nodeMCU device (functional/partial)

--- Ionic/src/app/modules/serial-terminal/pages/serial-terminal.component.ts
+++ Ionic/src/app/modules/serial-terminal/pages/serial-terminal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Serial } from '@ionic-native/serial/ngx';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-serial-terminal',
@@ -8,11 +9,63 @@ import { Serial } from '@ionic-native/serial/ngx';
 })
 export class SerialTerminalComponent implements OnInit {
 
-  constructor(private serial: Serial) { }
+  data = '';
+  connected = false;
+  str='';
+  fullStr = '';
+  codeInput;
 
+  constructor(private serial: Serial) {}
+  
   ngOnInit() {
+    setInterval(()=>{this.runSerialPort()},100);
   }
 
+  runSerialPort(){
+    this.serial.requestPermission().then(() => {
+      this.serial.open({
+        baudRate: 115200,
+        dataBits: 8,
+        stopBits: 1,
+        parity: 0,
+        dtr: true,
+        rts: true,
+        sleepOnPause: false
+      }).then(() => {
+        console.log('Serial connection opened');
+        this.connected=true;
+        this.serial.registerReadCallback().subscribe((data)=> {
+          // var enc = new TextDecoder("utf-8");
+          // this.data = enc.decode(data);
+          var view = new Uint8Array(data);
+          if(view.length >= 1) {
+            for(var i=0; i < view.length; i++) {
+                //if we received a \n, the message is complete, display it
+                if(view[i] == 13) {
+                    // check if the read rate correspond to the arduino serial print rate
+                    this.fullStr = this.fullStr + this.str + '\n';
+                    this.str = '';
+                }
+                // if not, concatenate with the begening of the message
+                else {
+                  var temp_str = String.fromCharCode(view[i]);
+                  var str_esc = escape(temp_str);
+                  this.codeInput = unescape(str_esc);
+                  this.str += unescape(str_esc);
+                }
+            }
+        }
+          this.data="[ok]";
+          this.str="-"+ this.str;
+          if(!this.data) this.data = "ERROR REGISTER READ CALLBACK";
+        });
+      });
+    }).catch((error: any) => {
+      this.connected=false;
+      this.data = "CONNECTION [ko]";
+      console.log(error)
+    });
+  }
 
 
 }
