8f89c9db4a423911beabc4e56d6a1d03e2567e4e
dteso <dtesodev@gmail.com>
2020-10-10 11:37:00 +0200

    serial terminal component using custom service

--- Ionic/src/app/modules/serial-terminal/components/terminal/serial-terminal.component.ts
+++ Ionic/src/app/modules/serial-terminal/components/terminal/serial-terminal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Serial } from '@ionic-native/serial/ngx';
+import { SerialData } from '../../../../services/models';
+import { CustomSerialService } from 'src/app/services/services';
 
 @Component({
   selector: 'app-serial-terminal',
@@ -8,70 +9,24 @@ import { Serial } from '@ionic-native/serial/ngx';
 })
 export class SerialTerminalComponent implements OnInit {
 
-  data = '';
-  connected = false;
-  str='';
-  fullStr = '';
-  codeInput;
-  message: '';
+  serialData:SerialData = {
+    data : '',
+    connected : false,
+    str:'',
+    fullStr : '',
+    codeInput: '',
+    message: '',
+  };
 
-  constructor(private serial: Serial) {}
+  constructor(private customSerialService: CustomSerialService) {}
   
   ngOnInit() {
-    setInterval(()=>{this.runSerialPort()},100);
+    setInterval(()=>{
+      this.customSerialService.runSerialPort();
+      this.customSerialService.getSerialData().then( res => {
+        this.serialData = res;
+      }).catch(console.log);
+    },100);
   }
-
-  runSerialPort(){
-    this.serial.requestPermission().then(() => {
-      this.serial.open({
-        baudRate: 115200,
-        dataBits: 8,
-        stopBits: 1,
-        parity: 0,
-        dtr: true,
-        rts: true,
-        sleepOnPause: false
-      }).then(() => {
-        console.log('Serial connection opened');
-        this.connected=true;
-        this.serial.registerReadCallback().subscribe((data)=> {
-          var view = new Uint8Array(data);
-          if(view.length >= 1) {
-            for(var i=0; i < view.length; i++) {
-                //if we received a \n, the message is complete, display it
-                if(view[i] == 13) {
-                    this.fullStr = this.fullStr + this.str + '\n';
-                    this.str = '';
-                }
-                // if not, concatenate with the begening of the message
-                else {
-                  var temp_str = String.fromCharCode(view[i]);
-                  var str_esc = escape(temp_str);
-                  this.codeInput = unescape(str_esc);
-                  this.str += unescape(str_esc);
-                }
-            }
-        }
-          this.data="[ok]";
-          this.str="-"+ this.str;
-          if(!this.data) this.data = "ERROR REGISTER READ CALLBACK";
-        });
-      });
-    }).catch((error: any) => {
-      this.connected=false;
-      this.data = "CONNECTION [ko]";
-      console.log(error)
-    });
-  }
-
-  showOptions(){
-    console.log("Options");
-  }
-
-  cleanConsole(){
-    this.fullStr='';
-    console.log("Clear Console");
-  }
-
 
 }
