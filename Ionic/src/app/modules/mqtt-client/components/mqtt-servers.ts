8e672d804f91490786dc0fee543a58fb3529e200
dteso <dtesodev@gmail.com>
2020-12-28 11:46:22 +0100

    full topics management for mqtt client

---
+++ Ionic/src/app/modules/mqtt-client/components/mqtt-servers.ts
@@ -0,0 +1,30 @@
+export const MQTT_SERVERS = [
+    {
+      name: 'Custom server edit',
+      hostname: '',
+      port: 0,
+      path: '/mqtt',
+      imgUrl: './assets/images/localhost.png'
+    },
+    {
+      name: 'Mosquitto',
+      hostname: 'test.mosquitto.org',
+      port: 8080,
+      path: '/mqtt',
+      imgUrl: './assets/images/mosquitto.png'
+    },
+    {
+      name: 'Emqx',
+      hostname: 'broker.emqx.io',
+      port: 8083,
+      path: '/mqtt',
+      imgUrl: './assets/images/emqx.png'
+    },
+    {
+      name: 'Hivemq',
+      hostname: 'broker.hivemq.com',
+      port: 8000,
+      path: '/mqtt',
+      imgUrl: './assets/images/hivemq.png'
+    },
+  ];
--- Ionic/src/app/modules/mqtt-client/pages/mqtt-client.component.ts
+++ Ionic/src/app/modules/mqtt-client/pages/mqtt-client.component.ts
@@ -4,6 +4,7 @@ import { MqttService } from 'ngx-mqtt';
 import { IMqttMessage } from 'ngx-mqtt/public-api';
 import { Subscription } from 'rxjs';
 import { MQTT_SERVICE_OPTIONS } from '../components/mqtt-options';
+import { MQTT_SERVERS } from '../components/mqtt-servers';
 
 @Component({
   selector: 'app-mqtt-client',
@@ -14,58 +15,30 @@ export class MqttClientComponent implements OnInit {
 
   private subscription: Subscription;
   topicname: any;
+  date:Date;
   msg: any;
   isConnected: boolean = false;
   @ViewChild('msglog', { static: true }) msglog: ElementRef;
   options = MQTT_SERVICE_OPTIONS;
+  mqttServers = MQTT_SERVERS;
   title = "MQTT Client";
   serverSelected = 0;
   host = '';
   port = null;
   lastTopicIndex = 0;
   msgToSend = '';
-
   topics = [];
+  deletedTopics = [];
 
   mqttForm: FormGroup;
 
-  mqttServers = [
-    {
-      name: 'Custom server edit',
-      hostname: '',
-      port: 0,
-      path: '/mqtt',
-      imgUrl: './assets/images/localhost.png'
-    },
-    {
-      name: 'Mosquitto',
-      hostname: 'test.mosquitto.org',
-      port: 8080,
-      path: '/mqtt',
-      imgUrl: './assets/images/mosquitto.png'
-    },
-    {
-      name: 'Emqx',
-      hostname: 'broker.emqx.io',
-      port: 8083,
-      path: '/mqtt',
-      imgUrl: './assets/images/emqx.png'
-    },
-    {
-      name: 'Hivemq',
-      hostname: 'broker.hivemq.com',
-      port: 8000,
-      path: '/mqtt',
-      imgUrl: './assets/images/hivemq.png'
-    },
-  ]
   constructor(
     private _mqttService: MqttService,
     private formBuilder: FormBuilder
   ) {
+    this.date=new Date();
     this.mqttForm = this.formBuilder.group({
-      topicname: [''],
-      msg: ['', Validators.required],
+      topicname: ['', Validators.required]
     });
     // TODO: Changing mqtt connection parameters dinamically
     this._mqttService.disconnect();
@@ -87,34 +60,49 @@ export class MqttClientComponent implements OnInit {
   }
 
   subscribeNewTopic(): void {
+    if(this.topics.filter(topic => topic.topic === this.mqttForm.controls.topicname.value).length > 0 || this.mqttForm.invalid){
+      return;
+    };
     console.log('inside subscribe new topic');
-    this.topics.push({ topic: this.mqttForm.controls.topicname.value, message: ''});
+    this.topics.push({ 
+      topic: this.mqttForm.controls.topicname.value, 
+      message: '', 
+      timestamp: this.date.getTime(),
+      allowPublish: false
+    });
     this.subscription = this._mqttService.observe(this.mqttForm.controls.topicname.value).subscribe((message: IMqttMessage) => {
+      if(this.deletedTopics.filter( topic => topic.topic === message.topic).length > 0){
+        this.subscription.unsubscribe();
+        let topicPos=0;
+        topicPos = this.deletedTopics.find( dt => dt.topic === message.topic);
+        this.deletedTopics.splice(topicPos,1);
+        return;
+      }
       let pos = 0;
       this.msg = message;
       console.log('msg: ', message);
       this.clear();
       let count = 0;
       this.topics.forEach( topic => {
-        if(topic.topic === message.topic){
-          pos=count;
-        }else{
-          count++;
-        }
-      })
+        topic.topic === message.topic? pos=count : count++;
+      });
+      this.topics[pos].topic = message.topic;
       this.topics[pos].message = message.payload.toString();
+      this.topics[pos].timestamp = this.date.getTime();
       this.lastTopicIndex = pos;
+      this.mqttForm.controls.topicname.setValue('');
       this.logMsg('Last MESSAGE received was: << ' + message.payload.toString() + '>> on TOPIC: << ' + message.topic +' >>');
     });
     this.clear();
     this.logMsg('Subscribed to topic: << ' + this.mqttForm.controls.topicname.value +' >>');
   }
 
-  sendmsg(topic:string, msg: string): void {
+  sendmsg(index:any, topic:string, msg: string): void {
     // use unsafe publish for non-ssl websockets
-    this._mqttService.unsafePublish(topic, msg, { qos: 1, retain: true })
+    this._mqttService.unsafePublish(topic, msg, { qos: 1, retain: true });
     this.msg = '';
     this.msgToSend = '';
+    this.topics[index].allowPublish = false;
   }
 
   logMsg(message): void {
@@ -127,6 +115,9 @@ export class MqttClientComponent implements OnInit {
 
   onSelectServer(evt){
     this.serverSelected = this.mqttServers.indexOf(evt.detail.value);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
     this._mqttService.disconnect();
     this.options.hostname = this.mqttServers[this.serverSelected].hostname;
     this.options.port = this.mqttServers[this.serverSelected].port;
@@ -137,6 +128,8 @@ export class MqttClientComponent implements OnInit {
     this.mqttForm.controls.msg.setValue('');
     this.host='';
     this.port='';
+    this.topics = [];
+    this.deletedTopics = [];
   }
 
   onModelChange(){
@@ -147,6 +140,11 @@ export class MqttClientComponent implements OnInit {
   connect(){
     this._mqttService.disconnect();
     this._mqttService.connect(this.options);
+  }
+
+  deleteTopic(index){
+    this.deletedTopics.push(this.topics[index]);
+    this.topics.splice(index, 1);
   }
 }
 
